5b5b48078387e81eb0eea8acab945be933e002f7
mmmmm1233212344 <abhaymilan8@gmail.com>
2023-01-11 18:01:39 +0530

    Added Commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,50 @@
-// 
-import React from 'react'
-import './App.css'
-import Die from './Die'
-import {v4 as uuid} from 'uuid'
-// 
+//
+import React from "react";
+import "./App.css";
+import Die from "./Die";
+import { v4 as uuid } from "uuid";
+//
 
 function App() {
-const [dice,setDice]=React.useState(createDice())
+  //Setup a state to store the value of the dice.
+  const [dice, setDice] = React.useState(createDice());
 
-
-function createDice(){
-  let arr=[];
-  for(let i=0;i<10;i++){
-   arr.push([
-
-   {
-
-     id:uuid(),
-     value: Math.ceil(Math.random()*6),
-     isHOld:true
+  //Create the function to create an object to the dice state
+  function createDice() {
+    let arr = [];
+    for (let i = 0; i < 10; i++) {
+      arr.push([
+        {
+          id: uuid(),//Creates A Unique Id
+          value: Math.ceil(Math.random() * 6),
+          isHOld: true,
+        },
+      ]);
     }
-    ]
-  ) 
+    return arr;
   }
-  return arr;
-}
-// console.log(dice)
-function diceRoll(){
-  setDice(createDice())
-}
-
-
-  
-  const dieElements = dice.map(die=>{
-    console.log(die)
-     return <Die value={die[0].value} isHOld={die[0].isHOld}/>
-  })
+  // console.log(dice)
+  //Create the function to change the values everytime the roll button is clicked
+  function diceRoll() {
+    setDice(createDice());
+  }
+  //Map through the values of the dice state and pass the value as props to the single component
+  const dieElements = dice.map((die) => {
+    console.log(die);
+    return <Die value={die[0].value} isHOld={die[0].isHOld} />;
+  });
 
   return (
     <div className="App">
-    <main className='Main'>
-    <div className="container">
-            {dieElements}
-        </div>
-       
-      
-     
+      <main className="Main">
+        <div className="container">{dieElements}</div>
 
-          <button className='roll' onClick={diceRoll}>ROLL</button>
-        
-    </main>
+        <button className="roll" onClick={diceRoll}>
+          ROLL
+        </button>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Die.jsx
+++ src/Die.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 export default function Die(props){
     // console.log(props)
+    
     const styles={
         backgroundColor:props.isHOld?"blue":"white"
     }
