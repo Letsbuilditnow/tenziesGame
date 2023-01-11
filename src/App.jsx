736403ba4b1f0b72da7e3ab0b67973e97e14ecb0
mmmmm1233212344 <abhaymilan8@gmail.com>
2023-01-11 17:57:49 +0530

    Added Object Including Value And Id

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import './App.css'
 import Die from './Die'
+import {v4 as uuid} from 'uuid'
 // 
 
 function App() {
@@ -11,10 +12,20 @@ const [dice,setDice]=React.useState(createDice())
 function createDice(){
   let arr=[];
   for(let i=0;i<10;i++){
-   arr.push(Math.ceil(Math.random()*6)) 
+   arr.push([
+
+   {
+
+     id:uuid(),
+     value: Math.ceil(Math.random()*6),
+     isHOld:true
+    }
+    ]
+  ) 
   }
   return arr;
 }
+// console.log(dice)
 function diceRoll(){
   setDice(createDice())
 }
@@ -22,7 +33,8 @@ function diceRoll(){
 
   
   const dieElements = dice.map(die=>{
-     return <Die value={die}/>
+    console.log(die)
+     return <Die value={die[0].value} isHOld={die[0].isHOld}/>
   })
 
   return (
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 export default function Die(props){
+    // console.log(props)
+    const styles={
+        backgroundColor:props.isHOld?"blue":"white"
+    }
     return(
         <div className="container">
-            <button>{props.value}</button>
+            <button style={styles}>{props.value}</button>
             
         </div>
     )
