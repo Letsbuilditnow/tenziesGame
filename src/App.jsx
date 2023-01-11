0cc3b3173037b2d4559860fc297e11317e86211f
mmmmm1233212344 <abhaymilan8@gmail.com>
2023-01-11 12:18:22 +0530

    Manually Setup Repetition Of The Dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 // 
 import React from 'react'
 import './App.css'
+import Die from './Die'
 // 
 
 function App() {
-  
+
+   
+  let arr=[1,2,3,4]
+  const dieElements = arr.map(die=>{
+     return <Die/>
+  })
 
   return (
     <div className="App">
     <main className='Main'>
     <div className="container">
-            <button>1</button>
-            <button>1</button>
-            <button>1</button>
-            <button>1</button>
-            <button>1</button>
-            <button>1</button>
-            <button>1</button>
-            <button>1</button>
-            <button>1</button>
-            <button>1</button>
+            {dieElements}
+          
         </div>
     </main>
     </div>
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,15 +1,10 @@
-// import React from "react";
+import React from "react";
 
-// export default function Die(){
-//     return(
-//         <div className="container">
-//             <button>1</button>
-//             <button>1</button>
-//             <button>1</button>
-//             <button>1</button>
-//             <button>1</button>
-//             <button>1</button>
-//             <button>1</button>
-//         </div>
-//     )
-// }
+export default function Die(){
+    return(
+        <div className="container">
+            <button>1</button>
+            
+        </div>
+    )
+}
