67691aef27707e1b415881b45c5f3952ce63c81f
mmmmm1233212344 <abhaymilan8@gmail.com>
2023-01-12 11:24:52 +0530

    Added The React-Confetti Package

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,18 @@ import { v4 as uuid } from "uuid";
 function App() {
   //Setup a state to store the value of the dice.
   const [dice, setDice] = React.useState(createDice());
+  const [tenzies,setTenzies]=React.useState(false)
+
+  React.useEffect(()=>{
+   let firstValue=dice[0].value;
+   let everyValue=dice.every(die=>{
+   return die.isHold && firstValue==die.value})
+   console.log(everyValue)
+   if(firstValue && everyValue ){
+    console.log("You Won")
+     setTenzies(true) 
+   }
+  },[dice])
 
   //Create the function to create an object to the dice state
   function createDice() {
@@ -17,7 +29,7 @@ function App() {
         {
           id: uuid(),//Creates A Unique Id
           value: Math.ceil(Math.random() * 6),
-          isHold: true,
+          isHold: false,
         },
       );
     }
@@ -37,11 +49,16 @@ function App() {
   // console.log(dice)
   //Create the function to change the values everytime the roll button is clicked
   function diceRoll() {
-    setDice(createDice());
+    setDice(prevDice=>{
+      return prevDice.map(die=>{
+        return die.isHold==true?die:{...die, id: uuid(), value: Math.ceil(Math.random() * 6)}
+      })
+    }
+      );
   }
   //Map through the values of the dice state and pass the value as props to the single component
   const dieElements = dice.map((die) => {
-    console.log(die);
+    // console.log(die);
     return <Die value={die.value} isHold={die.isHold} holdIt={()=>{holdIt(die.id)}} />;
   });
 
